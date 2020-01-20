45f89a764750eef6c27f500b0433b40b8f430a73
Arnaldo Liberal <arnaldo@ahcom.com.br>
2020-01-20 22:43:04 +0000

    updated file

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const beeJS       = require('./lib/beeJS')
 
+const config      = require('./config')
 const routes      = require('./routes')
 const models      = require('./models')
 const controllers = require('./controllers/index')
+
+beeJS.db(config.db)
+
+// TODO
